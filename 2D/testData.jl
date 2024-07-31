d5bab106c4bb3e17911bdbe2f1ac1bc3c3e1a707
Oscar <oscje382@student.liu.se>
2024-07-31 15:42:20 +0200

    Added more testData

--- 2D/testData.jl
+++ 2D/testData.jl
@@ -13,7 +13,7 @@ zdata_1 = [0 0 0 0 0 0 0;
 xPlotData_1 = repeat(xdata_1,length(xdata_1))
 yPlotData_1 = repeat(xdata_1,length(ydata_1))
 zPlotData_1 = vec(zdata_1)
-
+# ----------------------------------------------------------------
 xdata_2 = [0,1,2,3,4,5,6]
 ydata_2 = [0,1,2,3,4,5,6]
 zdata_2 = [0 0 0 1 1 1 1;
@@ -24,6 +24,20 @@ zdata_2 = [0 0 0 1 1 1 1;
            0 0 0 1 1 1 1;
            0 0 0 1 1 1 1]
 
-xPlotData_2 = repeat(xdata_2,length(xdata_2))
-yPlotData_2 = repeat(xdata_2,length(ydata_2))
-zPlotData_2 = vec(zdata_2)
+xPlotData_2 = repeat(xdata_3,length(xdata_3))
+yPlotData_2 = repeat(xdata_3,length(ydata_3))
+zPlotData_2 = vec(zdata_3)
+# ----------------------------------------------------------------
+xdata_3 = [0,1,2,3,4,5,6]
+ydata_3 = [0,1,2,3,4,5,6]
+zdata_3 = [1 1 1 1 1 1 1;
+           1 1 1 1 1 1 1;
+           1 1 1 1 1 1 1;
+           1 1 1 1 1 1 1;
+           1 1 1 1 1 1 1;
+           1 1 1 1 1 1 1;
+           1 1 1 1 1 1 1]
+
+xPlotData_3 = repeat(xdata_3,length(xdata_3))
+yPlotData_3 = repeat(xdata_3,length(ydata_3))
+zPlotData_3 = vec(zdata_3)
