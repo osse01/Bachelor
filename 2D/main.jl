34382b44c296354ceb38215884d069b89605d2d2
Oscar <oscje382@student.liu.se>
2024-07-31 15:39:42 +0200

    deleted old stuff

--- 2D/main.jl
+++ 2D/main.jl
@@ -6,35 +6,20 @@ xdata = xdata_2
 ydata = ydata_2
 zdata = zdata_2
 
-# len = 4;
-# xdata = [0,1,2,3,4,5,6]
-# ydata = [0,1,2,3,4,5,6]
-# zdata = [0 0 0 0 0 0 0;
-#          0 0 0 0 0 0 0;
-#          0 0 1 1 1 0 0;
-#          0 0 1 1 1 0 0;
-#          0 0 1 1 1 0 0;
-#          0 0 0 0 0 0 0;
-#          0 0 0 0 0 0 0]
 N = 20
 M = N
 spline = biCubicSpline(xdata, ydata, zdata, N, 0.0001)
-# bx = spline.bx
-# by = spline.by
+
 x = range(xdata[1], xdata[end], N)
 y = range(ydata[1], ydata[end], M)
 z = evaluate(spline, N, M)
 
 # Create the plot
 #gr()
-# xdata = [0,1,2,3,4,5,6, 0,1,2,3,4,5,6, 0,1,2,3,4,5,6, 0,1,2,3,4,5,6, 0,1,2,3,4,5,6, 0,1,2,3,4,5,6, 0,1,2,3,4,5,6]
-# ydata = [0,0,0,0,0,0,0, 1,1,1,1,1,1,1, 2,2,2,2,2,2,2, 3,3,3,3,3,3,3, 4,4,4,4,4,4,4, 5,5,5,5,5,5,5, 6,6,6,6,6,6,6]
-# zdata = vec(zdata)
 xdata = xPlotData_2
 ydata = yPlotData_2
 zdata = zPlotData_2
 
-# , quiver = (vec(bx), vec(by), ones(1,len))
 plot(x, y, z, seriestype=:surface, label="Lavery spline")
 plot!(xdata, ydata, zdata, seriestype=:scatter, label="data points", camera=(0,90))
 xaxis!("x")  # Set x-axis label
